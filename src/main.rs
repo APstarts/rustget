6e14f606102efc40e3920f8cc9fcfab24b5dcaf6
Anurag Pal <tanmaypal123@outlook.com>
2026-07-11 13:16:23 +0530

    added permits for limiting the number of concurrent tasks

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,25 @@ mod downloader;
 mod options;
 mod progress;
 mod utils;
-
 use anyhow::Result;
 use clap::Parser;
+use std::sync::Arc;
 
 use downloader::download;
 use options::DownloadOptions;
+use tokio::sync::Semaphore;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    const MAX_CONCURRENT_DOWNLOADS: usize = 4;
     let options = DownloadOptions::parse();
+    let semaphore = Arc::new(Semaphore::new(MAX_CONCURRENT_DOWNLOADS)); //to limit the number of concurrent tasks
+    //allowed at a time.
 
     let mut handles = Vec::new();
 
     for url in options.urls {
+        let semaphore_arcloned = Arc::clone(&semaphore);
         let handle = tokio::spawn(async move {
             //tokio spawn
             //creates a
@@ -25,6 +30,10 @@ async fn main() -> Result<()> {
             //thread::spawn
             //creates an
             //OS thread
+            let _permit = semaphore_arcloned.acquire_owned().await?; //When should a task a permit? Immediately
+            //before download(url) Why? Because waiting
+            //for  a permit is asynchronous. The task
+            //can happily exist while waiting.
             download(url).await
         });
         handles.push(handle);
--- src/options.rs
+++ src/options.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
+#[command(author, version, about)]
 pub struct DownloadOptions {
     pub urls: Vec<String>,
 }
