ccb539c65ef2f7d91c955b5303870b8978be0f99
Anurag Pal <tanmaypal123@outlook.com>
2026-07-18 13:45:50 +0530

    updated progress tracker code

--- src/progress.rs
+++ src/progress.rs
@@ -1,22 +1,27 @@
+use std::sync::{
+    Arc,
+    atomic::{AtomicU64, Ordering},
+};
+
 pub struct ProgressTracker {
-    downloaded: u64,
+    downloaded: Arc<AtomicU64>,
     total: Option<u64>,
 }
 
 impl ProgressTracker {
     pub fn new(total: Option<u64>) -> Self {
         Self {
-            downloaded: 0,
+            downloaded: Arc::new(AtomicU64::new(0)),
             total,
         }
     }
 
     pub fn add_bytes(&mut self, bytes: u64) {
-        self.downloaded += bytes;
+        self.downloaded.fetch_add(bytes, Ordering::Relaxed);
     }
 
     pub fn downloaded(&self) -> u64 {
-        self.downloaded
+        self.downloaded.load(Ordering::Relaxed)
     }
 
     pub fn total(&self) -> Option<u64> {
@@ -25,6 +30,6 @@ impl ProgressTracker {
 
     pub fn percentage(&self) -> Option<f64> {
         self.total
-            .map(|total| (self.downloaded as f64 / total as f64) * 100.0)
+            .map(|total| (self.downloaded() as f64 / total as f64) * 100.0)
     }
 }
